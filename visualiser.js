939d379af2c29bbfb8bdba48e32ce557498ceb5a
Aviskar KC <aviskarkc10@gmail.com>
2018-02-22 16:48:33 +0545

    Add support for changing song

--- visualiser.js
+++ visualiser.js
@@ -1,17 +1,19 @@
 function Visualiser() {
   var context, src, analyser, dataArray = [], barChart, circle;
 
+  context = new AudioContext();
+  console.log(context);
+  src = context.createMediaElementSource(audio);
+  console.log(context);
+  analyser = context.createAnalyser();
+
+  src.connect(analyser);
+  analyser.connect(context.destination);
+  analyser.fftSize = 256;
+
   this.init = function () {
     circle = new Circle();
     barChart = new BarChart();
-
-    context = new AudioContext();
-    src = context.createMediaElementSource(audio);
-    analyser = context.createAnalyser();
-
-    src.connect(analyser);
-    analyser.connect(context.destination);
-    analyser.fftSize = 256;
 
     dataArray = new Uint8Array(analyser.frequencyBinCount);
     getFrequency();
